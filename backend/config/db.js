55f0f89636da233666456ade5f3b53c98454a2c2
Bilalik369 <bilal11iken@gmail.com>
2025-04-20 22:26:51 +0100

    Ajouter la connexion à MongoDB avec Mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+
+const connectDb = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("MongoDB connecté");
+    }catch (err){
+        console.error('Erreur de connexion à MongoDB:', err);
+        process.exit(1);
+
+    }
+};
+
+module.exports = connectDb;
+
