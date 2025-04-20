73b25faaca78e797d6890d4ccab060878d28231b
Bilalik369 <bilal11iken@gmail.com>
2025-04-20 22:58:57 +0100

    Ajouter le contrôleur sendMessage pour enregistrer les messages de contact

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,24 @@
+import Message  from "../models/Message";
+
+
+export const sendMessage = async (req , res) =>{
+    
+    const {name , email ,message} = req.body;
+    if (!name || !email || !message) {
+        return res.status(400).json({
+          success: false,
+          error: 'Tous les champs sont requis : nom, email et message.',
+        });
+      }
+    
+      try{
+        const newMessage = new Message ({name , email , message});
+        await newMessage.save();
+        res.status(201).json({ success : true ,  message: 'Message envoyé avec succès !'});
+
+      }catch (err){
+        res.status(500).json({ success: false, error: 'Échec de l\'envoi du message.' });
+      }
+        
+    
+};
